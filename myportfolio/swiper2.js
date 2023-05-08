49950a72a184e6ff83dee6fd2c76fa271cf9df01
razannf <99996998+razannf@users.noreply.github.com>
2023-05-08 22:15:33 +0300

    Add files via upload

---
+++ myportfolio/swiper2.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.swiper-container', {
+	// init: false,
+	navigation : {
+		nextEl : '.swiper-button-next',
+		prevEl : '.swiper-button-prev',
+	},
+	slidesPerView : 'auto',
+	loop : true,
+  spaceBetween : 34
+});
